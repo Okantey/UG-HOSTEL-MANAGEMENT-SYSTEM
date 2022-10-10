42b946bba86a3e7551e7170f511ec29c8d4e63ed
Okantey <nunoogideon19@gmail.com>
2022-10-10 18:46:25 +0000

    updated records

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function validateForm() {
-  let x = document.forms["loginForm"]["email"]["password"].value;
+  let x = document.forms["loginForm"]["email" && ["password"]].value;
   if (x == "") {
     alert("Name must be filled out");
     return false;
