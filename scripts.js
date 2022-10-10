0ff9b06189ddb523b8faca852a19a6062c2c7b03
Okantey <nunoogideon19@gmail.com>
2022-10-10 19:09:22 +0000

    working biometric

--- scripts.js
+++ scripts.js
@@ -5,3 +5,15 @@ function validateForm() {
     return false;
   }
 }
+
+// biometrics
+
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#biometric-photo").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
